7c80a4d1190b7de484280c1428c1078bf29ef576
junnmerks <junnmerks@yahoo.com>
2020-04-15 16:27:39 +1200

    task 1 done

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
+var principal = 200000;
+let interestRate = 0.05;
+const years = 30;
+const name = "Junnifer Mercado";
 
 
 
