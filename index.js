0b057b2d1c40374e5ea58105d3c1517fa4d17e36
unknown <clay2000lbs@gmail.com>
2020-04-27 20:35:07 -0500

    Start of JavaScript- Foundations

--- index.js
+++ index.js
@@ -4,7 +4,13 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+let principal = 200000;
 
+let interestRate = 0.05;
+
+let years = 30;
+
+const name = clayton;
 
 
 
