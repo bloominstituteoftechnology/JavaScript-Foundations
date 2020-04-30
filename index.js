731e7b185a2f13a2cffab1397d9822b0a8c0466b
Jr08151 <Jr08151@users.noreply.github.com>
2020-04-30 19:04:12 -0400

    d

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ When your math is correct, monthlyRate will equal 1073.64
 let monthlyRate = principal * (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), periods))/(Math.pow((1 + monthlyInterestRate), periods) - 1);
 console.log(monthlyRate);
 
+let numerator = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), periods);
+let denominator = Math.pow((1 + monthlyInterestRate), periods) - 1;
 
+let monthlyRate =  principal * (numerator/denominator);
+console.log(monthlyRate)
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
