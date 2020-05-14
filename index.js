759c71d960c0e7ff469a94f7774a09e23eb88275
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-05-15 00:59:30 +0100

    Task 1 completed

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
+const principal = 200000;
+const interestRate = 0.05;
+const years = 30;
+const name = Femi;
 
 
 
