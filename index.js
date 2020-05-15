55a67197d7c40a7e597589765e6a95202480e18e
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-05-15 01:40:06 +0100

    Mortgage calculator passed

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
-*/
+ */
 const principal = 200000;
 const interestRate = 0.05;
 const years = 30;
 const name = "Femi";
-
-
-
 
 // 🏡 Task 1.5: Simple Math
 /* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate. 
@@ -18,7 +15,7 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
-const monthlyInterestRate = interestRate/12;
+const monthlyInterestRate = interestRate / 12;
 console.log(monthlyInterestRate);
 const periods = years * 12;
 console.log(periods);
@@ -33,23 +30,23 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 // M = P [ I ( 1 + I )^N ] / [ ( 1 + I )^N – 1 ]
-// monthlyRate = 
-const numerator = (monthlyInterestRate * Math.pow((monthlyInterestRate + 1),periods ));
-const denominator = (Math.pow((monthlyInterestRate + 1),periods) - 1);
-const monthlyRate = principal * (numerator/denominator);
+// monthlyRate =
+const numerator =
+  monthlyInterestRate * Math.pow(monthlyInterestRate + 1, periods);
+const denominator = Math.pow(monthlyInterestRate + 1, periods) - 1;
+const M = principal * (numerator / denominator);
+let monthlyRate = M.toPrecision(6);
 console.log(monthlyRate);
-
-
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
-
-
-
-
+function mortgageCalculator() {
+  console.log(`${name}, your monthly rate is ${monthlyRate}`);
+}
+mortgageCalculator();
 
 // 🏡 Task 4: Arguments and Parameters
 /* Substitute the variables in your functions for parameters such that you can substitute `P`, `I`, and `N` when you call the function.
@@ -58,18 +55,11 @@ For example,
 mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
-
-
-
-
 // 🏡 Task 5: Conditionals
 /* Add another paramter to your function called credit score. This parameter will be a number between 0 and 800 (a credit score).
 
 Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
 */
-
-
-
 
 // 🏡 Task 6: Loops
 /* Write a new function called variableInterestRate. This function should be the same as mortgageCalculator, except it should console.log the monthly payment for 10 different interest rates at 0.5% increments plus or minus 2% from the inputted interest rate. Complete these calculations using a for loop.
@@ -87,20 +77,14 @@ For example, variableInterestRate(200000, 0.04, 30) should console.log:
 "{Name}, with an interest rate of 0.06, your monthly rate is $1199"
 */
 
-
-
-
 // 🌟🌟🌟 STRETCH 🌟🌟🌟//
 
 /* Attempt any of the stretch goals below once you have finished the work above. Remember as always, these may require additional research beyond what you learned today */
 
 /*  🏡 Add  `Property Tax`, `Homeowner's insurance` and `HOA fees` as parameters in your function to calculate total monthly spending on housing */
 
-
 /* 🏡 Build a calculator function that accepts `monthly payment` and `interest rate` and returns the maximum loan that a person could afford */
 
-
 /* 🏡 Explore using `window.prompt()` to allow a user to input parameters in the browser */
-
 
 /* 🏡  Refactor your `variableInterestRate()` function to accept an array of interest rates (make sure to copy and paste as to not lose your work!) */
