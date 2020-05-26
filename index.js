96b13619c214ac85cab3f46dfae0731df05847d4
David Veele <you@example.com>
2020-05-26 01:45:02 -0400

    part one completed

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
-
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+const name = "Shenandoah Veele";
 
 
 // 🏡 Task 1.5: Simple Math
@@ -15,19 +16,22 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
-
+let periods = years * 12;
+let monthlyInterestRate = interestRate / 12;
 
 
 
 // 🏡 Task 2: Harder Math
 /* Create your calculator! Use the formula in the ReadMe to run calculations on your numbers. Save the final value into a variable called monthlyRate.
-
+M = P [ I ( 1 + I )^N ] / [ ( 1 + I )^N – 1 ]
+mortgage = Principle [interest(1 + Interest)^periods] / [(1 + I)^periods -1]
 Hint: while these calculations can be done in one line, it might be helpful to create a variable called "numerator" to calculate the numerator, and another called "denominator" to calculate the denominator 
 
 Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+
 
 
 
