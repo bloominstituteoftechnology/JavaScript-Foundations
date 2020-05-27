dd18d4b7c3475102c3b9c3e8c907c44f0bd2a4df
Tyler Taylor <61921198+TylerOliverTaylor@users.noreply.github.com>
2020-05-26 23:24:50 -0400

    Task 1-2

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
+let principal = 200000
+let interestRate = 0.05
+let years = 30
+let name = "Tyler"
 
 
 
@@ -16,7 +19,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 Create another variable called `periods` and give it the value of years*12.
 */
 
-
+let monthlyInterestRate = interestRate / 12
+console.log(monthlyInterestRate)
 
 
 // 🏡 Task 2: Harder Math
