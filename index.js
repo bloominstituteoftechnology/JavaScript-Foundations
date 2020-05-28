63da6c9c659e95ae6aa37cee5103902a0e9f8bb9
Joshua Cheney <joshua@thecheney.family>
2020-05-27 18:35:27 -0700

    Initial work done for Task 2, math isn't calculating for Task 3

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-var principal = '200000';
-var annualInterestRate = '0.05';
-var years = '30';
-
+let principal = '200000';
+let annualInterestRate = '0.05';
+let years = '30';
+const myName = 'Joshua Cheney';
 
 // 🏡 Task 1.5: Simple Math
 /* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate.
@@ -15,8 +15,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
-var monthlyInterestRate = annualInterestRate / 2;
-var periods = years * 12;
+const monthlyInterestRate = annualInterestRate / 2;
+const periods = years * 12;
 
 
 // 🏡 Task 2: Harder Math
@@ -28,19 +28,19 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
-
-
-
+const numerator = monthlyInterestRate*(Math.pow((1 + monthlyInterestRate),periods));
+const denominator = (Math.pow((1 + monthlyInterestRate),periods)) - 1;
+const monthlyRate = (principal*numerator/denominator).toFixed(2);
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
-
-
-
-
+function mortgageCalculator() {
+    console.log(`${myName}, your monthly rate is ${monthlyRate}`);
+  }
+  mortgageCalculator();
 
 // 🏡 Task 4: Arguments and Parameters
 /* Substitute the variables in your functions for parameters such that you can substitute `P`, `I`, and `N` when you call the function.
@@ -50,16 +50,11 @@ mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
 
-
-
-
 // 🏡 Task 5: Conditionals
 /* Add another paramter to your function called credit score. This parameter will be a number between 0 and 800 (a credit score).
 
 Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
 */
-
-
 
 
 // 🏡 Task 6: Loops
@@ -77,8 +72,6 @@ For example, variableInterestRate(200000, 0.04, 30) should console.log:
 "{Name}, with an interest rate of 0.055, your monthly rate is $1136"
 "{Name}, with an interest rate of 0.06, your monthly rate is $1199"
 */
-
-
 
 
 // 🌟🌟🌟 STRETCH 🌟🌟🌟//
