d2b106ee6930681b12bd989656c9370a5536b21d
Angie Lawrence <43270346+Angie0488@users.noreply.github.com>
2020-06-17 12:40:05 -0400

    completed tasks 1 and 2

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+const name = "Angie";
 
 
 
@@ -14,7 +18,8 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = interestRate / 12;
+let periods = years * 12;
 
 
 
