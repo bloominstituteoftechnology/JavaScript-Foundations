2c44e414ad2d50d94d8556faf1275779332d16bb
halliwell2046 <halliwell2046@gmail.com>
2020-06-18 19:40:58 -0400

    first js commit

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+let principal = 200000;
+let intRate = 0.05;
+let years = 30;
+const name = "Alice Frazier";
+
 
 
 
@@ -15,6 +20,11 @@
 (2) Create another variable called `periods` and give it the value of years*12.
 */
 
+let monthlyInterestRate = intRate/12;
+console.log(monthlyInterestRate);
+
+let periods = years*12;
+console.log(periods);
 
 
 
