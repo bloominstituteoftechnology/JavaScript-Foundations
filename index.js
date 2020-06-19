305b8178cedda43d7e4cf2c6dd34214c04c3040f
extrude57 <gamemeup8@protonmail.com>
2020-06-18 18:45:11 -0700

    Make mortgage calculator

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let years = 30;
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = interest;
 
 
 
