8dbc04d709eaaa09510900ee29e4f0421a1dae9b
halliwell2046 <halliwell2046@gmail.com>
2020-06-19 18:32:37 -0400

    task 2 commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+
 // 🌟🌟🌟 M V P 🌟🌟🌟//
 
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-let principal = 200000;
-let intRate = 0.05;
-let years = 30;
-const name = "Alice Frazier";
+let principal = 200000
+let intRate = 0.05
+let years = 30
+const name = 'Alice Frazier'
+
+console.log(name)
 
 
 
@@ -20,10 +23,10 @@ const name = "Alice Frazier";
 (2) Create another variable called `periods` and give it the value of years*12.
 */
 
-let monthlyInterestRate = intRate/12;
+const monthlyInterestRate = intRate/12;
 console.log(monthlyInterestRate);
 
-let periods = years*12;
+const periods = years*12;
 console.log(periods);
 
 
@@ -44,8 +47,17 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+const n1 = 1 + monthlyInterestRate ^ intRate;
+console.log(n1);
 
+const numerator = principal * n1 * monthlyInterestRate;
+console.log(numerator)
 
+const denominator = n1 -1;
+console.log(denominator)
+
+const monthlyRate = numerator/denominator;
+console.log(monthlyRate)
 
 
 // 🏡 Task 3: Function
