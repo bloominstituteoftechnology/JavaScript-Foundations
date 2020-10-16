1bde6f5930c39795bf9ac1ddf875994ea512247a
Trevor Locke <locke.trevor@gmail.com>
2020-10-16 00:06:17 -0500

    Finished Task 2

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30; 
+let name = 'Trevor'
 
 
 
@@ -14,6 +18,8 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
+let monthlyInterestRate = (interestRate / 12);
+let periods = (years * 12);
 
 
 
@@ -35,7 +41,11 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
-
+let n1 = Math.pow ((1+ monthlyInterestRate), periods);
+ let numerator = (principal * n1 * monthlyInterestRate)
+ let denominator = (n1 - 1);
+ let monthlyRate = (numerator / denominator)
+ console.log(monthlyRate)
 
 
 // 🏡 Task 3: Function
